fb97953dfea0320c3918b9cfe27540abe8c7d8bb
Marko Pojer <marko.pojer@outlook.com>
2021-07-29 22:24:35 +0200

    prvi

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+        
+
+  console.log("esdsdggsgfsgfgf");
+  alert("dsdgfsgfs");
